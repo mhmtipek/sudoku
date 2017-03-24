beb56778333bf0304edcd58192859d919014fc39
mehmet <mhmtipek@gmail.com>
2017-03-24 18:53:30 +0300

    load Turksish translation if system language is Turkish

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    QScopedPointer<QTranslator> translator;
+    if (QLocale::system().language() == QLocale::Turkish) {
+        translator.reset(new QTranslator);
+        if (!translator->load(":/sudoku_tr.qm"))
+            qDebug() << "Failed to load translation";
+        else
+            app.installTranslator(translator.data());
+    }
+
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("GameControl", &GameControlProxy::instance());
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
