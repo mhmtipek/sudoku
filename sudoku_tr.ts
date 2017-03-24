4b7fdc1b7b4af26b11bc1f1d0a736c9bcf38eae4
mehmet <mhmtipek@gmail.com>
2017-03-24 18:52:58 +0300

    added Turkish translation file

---
+++ sudoku_tr.ts
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tr_TR">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="qml/AboutPage.qml" line="21"/>
+        <source>Author</source>
+        <translation>Geliştirici</translation>
+    </message>
+    <message>
+        <location filename="qml/AboutPage.qml" line="41"/>
+        <source>This app is developed by Mehmet Ipek.</source>
+        <translation>Bu uygulama Mehmet İpek tarafından geliştirilmiştir.</translation>
+    </message>
+    <message>
+        <location filename="qml/AboutPage.qml" line="50"/>
+        <location filename="qml/AboutPage.qml" line="64"/>
+        <location filename="qml/AboutPage.qml" line="104"/>
+        <location filename="qml/AboutPage.qml" line="110"/>
+        <location filename="qml/AboutPage.qml" line="116"/>
+        <location filename="qml/AboutPage.qml" line="122"/>
+        <location filename="qml/AboutPage.qml" line="129"/>
+        <source>See:</source>
+        <translation>Bkz:</translation>
+    </message>
+    <message>
+        <location filename="qml/AboutPage.qml" line="58"/>
+        <source>Source code is hosted at github and licensed under MIT.</source>
+        <translation>Kaynak kodları Github&apos;da tutulmaktadır ve MIT lisansı ile lisanslanmıştır.</translation>
+    </message>
+    <message>
+        <location filename="qml/AboutPage.qml" line="78"/>
+        <source>Images</source>
+        <translation>Görseller</translation>
+    </message>
+    <message>
+        <location filename="qml/AboutPage.qml" line="94"/>
+        <source>Images used in this app are provided by resources below. I thank all of them for licensing their work free use. I made some changes on these images. For example: Changing color or rounding corners.</source>
+        <translation>Bu uygulamadaki görseller aşağıdaki kaynaklardan sağlanmıştır. İşlerini özgür kullanım için lisansladıkları için hepsine teşekkür ederim. Görsellerin bazılarında değişiklikler yaptım. Örnek: Renk değiştirme ve köşeleri yuvarlaştırma.</translation>
+    </message>
+</context>
+<context>
+    <name>BoardSlotsDialog</name>
+    <message>
+        <location filename="qml/BoardSlotsDialog.qml" line="48"/>
+        <source>Empty slot</source>
+        <translation>Boş</translation>
+    </message>
+</context>
+<context>
+    <name>GameTypePage</name>
+    <message>
+        <location filename="qml/GameTypePage.qml" line="12"/>
+        <source>Standard</source>
+        <translation>Standard</translation>
+    </message>
+    <message>
+        <location filename="qml/GameTypePage.qml" line="14"/>
+        <source>Irregular</source>
+        <translation>Düzensiz</translation>
+    </message>
+</context>
+<context>
+    <name>Globals</name>
+    <message>
+        <location filename="qml/Extra/Globals.qml" line="9"/>
+        <location filename="qml/Extra/Globals.qml" line="34"/>
+        <source>Hardest</source>
+        <translation>Çok zor</translation>
+    </message>
+    <message>
+        <location filename="qml/Extra/Globals.qml" line="13"/>
+        <location filename="qml/Extra/Globals.qml" line="32"/>
+        <source>Hard</source>
+        <translation>Zor</translation>
+    </message>
+    <message>
+        <location filename="qml/Extra/Globals.qml" line="17"/>
+        <location filename="qml/Extra/Globals.qml" line="30"/>
+        <source>Normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <location filename="qml/Extra/Globals.qml" line="21"/>
+        <location filename="qml/Extra/Globals.qml" line="28"/>
+        <source>Easy</source>
+        <translation>Kolay</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="qml/MainPage.qml" line="34"/>
+        <source>New game</source>
+        <translation>Yeni oyun</translation>
+    </message>
+    <message>
+        <location filename="qml/MainPage.qml" line="35"/>
+        <source>Load game</source>
+        <translation>Oyun yükle</translation>
+    </message>
+    <message>
+        <location filename="qml/MainPage.qml" line="36"/>
+        <source>Scoreboard</source>
+        <translation>Skor tabelası</translation>
+    </message>
+    <message>
+        <location filename="qml/MainPage.qml" line="85"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="qml/MainPage.qml" line="86"/>
+        <source>Failed to load game</source>
+        <translation>Oyun yüklenemedi</translation>
+    </message>
+</context>
+<context>
+    <name>ScoreBoardPage</name>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="35"/>
+        <source>Easy</source>
+        <translation>Kolay</translation>
+    </message>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="38"/>
+        <source>Normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="41"/>
+        <source>Hard</source>
+        <translation>Zor</translation>
+    </message>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="44"/>
+        <source>Hardest</source>
+        <translation>Çok zor</translation>
+    </message>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="54"/>
+        <source>%1m %2s</source>
+        <translation>%1d %2s</translation>
+    </message>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="56"/>
+        <source>%1s</source>
+        <translation>%1s</translation>
+    </message>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="170"/>
+        <source>No score</source>
+        <translation>Skor yok</translation>
+    </message>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="223"/>
+        <source>Please confirm</source>
+        <translation>Lütfen onaylayın</translation>
+    </message>
+    <message>
+        <location filename="qml/ScoreBoardPage.qml" line="224"/>
+        <source>Are you sure to reset all scores?</source>
+        <translation>Tüm skorları silmek istediğinize emin misiniz?</translation>
+    </message>
+</context>
+<context>
+    <name>StandardGameConfigurationPage</name>
+    <message>
+        <location filename="qml/StandardGameConfigurationPage.qml" line="67"/>
+        <source>* May take a long time (~20 seconds)</source>
+        <translation>* Uzun sürebilir (~20 saniye)</translation>
+    </message>
+</context>
+<context>
+    <name>StandardSudokuInitialBoardCreator</name>
+    <message>
+        <location filename="standardsudokuinitialboardcreator.cpp" line="98"/>
+        <source>Creating board</source>
+        <translation>Tahta oluşturuluyor</translation>
+    </message>
+    <message>
+        <location filename="standardsudokuinitialboardcreator.cpp" line="101"/>
+        <source>Adjusting difficulty</source>
+        <translation>Zorluk ayarlanıyor</translation>
+    </message>
+</context>
+<context>
+    <name>SudokuGamePage</name>
+    <message>
+        <location filename="qml/SudokuGamePage.qml" line="64"/>
+        <source>%1m %2s</source>
+        <translation>%1d %2s</translation>
+    </message>
+    <message>
+        <location filename="qml/SudokuGamePage.qml" line="66"/>
+        <source>%1s</source>
+        <translation>%1s</translation>
+    </message>
+    <message>
+        <location filename="qml/SudokuGamePage.qml" line="291"/>
+        <source>Congrats!</source>
+        <translation>Tebrikler!</translation>
+    </message>
+    <message>
+        <location filename="qml/SudokuGamePage.qml" line="308"/>
+        <source>Game is finished!</source>
+        <translation>Oyun bitti!</translation>
+    </message>
+    <message>
+        <location filename="qml/SudokuGamePage.qml" line="378"/>
+        <location filename="qml/SudokuGamePage.qml" line="417"/>
+        <source>Confirm</source>
+        <translation>Onay</translation>
+    </message>
+    <message>
+        <location filename="qml/SudokuGamePage.qml" line="379"/>
+        <source>Game is not finished. Are you sure to exit?</source>
+        <translation>Oyun henüz bitmedi. Çıkmak istediğinize emin misiniz?</translation>
+    </message>
+    <message>
+        <location filename="qml/SudokuGamePage.qml" line="418"/>
+        <source>This slot is not empty. Are you sure to replace?</source>
+        <translation>Bu saklama alanı boş değil. Mevcut kayıt ile değiştirilsin mi?</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="qml/main.qml" line="10"/>
+        <source>Sudoku</source>
+        <translation>Sudoku</translation>
+    </message>
+</context>
+</TS>
